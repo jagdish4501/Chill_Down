7f71040dbfc2e8a30ed5e0d40ac9bbdabeff7e33
jagdish <pkjagdish4501@gmail.com>
2023-03-26 07:17:10 +0530

    final commit

--- server.py
+++ server.py
@@ -1,18 +1,26 @@
 import numpy as np
+import pandas as pd
 from flask import Flask, request, jsonify, render_template
 import joblib
 import json
+# blog api
 blog = []
 with open('blog.json') as f:
     blog = json.load(f)
 
-
+# creating Flask app
 app = Flask(__name__)
-model = joblib.load('ML_Model/svm_model.pkl')
+# loading model from pkl
+model_svm = joblib.load('ML_Model/svm_model.pkl')
+model_npl = joblib.load('ML_Model/nlp_model.pkl')
+count_vectorizer = joblib.load('ML_Model/cnt_vec.pkl')
+# minimum attribute value in svm_model
 mn = [0, 0, 0, 0, 0, 0, 0, 1, 0, 0, 1, 0, 1, 0, 0, 1, 0, 0, 0, 0, 1]
+# differece of max and min attribute value
 dif = [5, 10, 10, 10, 10, 10, 5, 1, 10,
        10, 9, 10, 9, 10, 10, 1, 10, 10, 10, 1]
 
+# bare minimum condition
 bare_min = [2, 2, 3, 3, 4, 2, 1, 1, 4, 1, 3, 1, 6, 0, 1, 1, 3, 1, 4, 0]
 
 Attribute = {
@@ -37,10 +45,14 @@ Attribute = {
     18: "WEEKLY_MEDITATION",
     19: "WORK_LIFE_BAL"}
 
-
-@app.route('/form')
-def form():
-    return render_template('form.html')
+lev_dep = {0: "Finanacial issue",
+           1: "Sleep Cycle Disturbance",
+           2: "Delay in Goals",
+           3: "Lack of Achievement",
+           4: "Introvert/less Socialise",
+           5: "Poor Relationships",
+           6: "Diet",
+           7: "Lack of Concentration"}
 
 
 @app.route('/')
@@ -48,27 +60,45 @@ def home():
     return render_template('index.html')
 
 
-@app.route('/predict', methods=['POST'])
+@app.route('/describe', methods=['GET', 'POST'])
+def describe():
+    if request.method == 'GET':
+        return render_template('desc.html')
+    elif request.method == 'POST':
+        data = [request.form['situation']]
+        data = pd.Series(data)
+        cv = count_vectorizer.transform(data)
+        print(cv.shape)
+        pred = model_npl.predict(cv)
+        print(pred)
+        return render_template('blog.html', prediction_text='Stress Genrated DueTo : {}'.format(lev_dep[pred[0]]))
+    else:
+        return "Method Not Allowed"
+
+
+@app.route('/predict', methods=['GET', 'POST'])
 def predict():
-    '''
-    For rendering results on HTML GUI
-    '''
-    responsible_idx = []
-    data = [float(x) for x in request.form.values()]
-    for idx in range(20):
-        if data[idx]-bare_min[idx] < 0:
-            responsible_idx.append(idx)
-    for idx in range(20):
-        data[idx] = (data[idx]-mn[idx])/dif[idx]
-    final_features = np.array(data).reshape(1, -1)
-    prediction = model.predict(final_features)
-    prediction_list = prediction.tolist()
-    local_blog = []
-    print(len(responsible_idx))
-    for idx in range(len(responsible_idx)):
-        local_blog.append(blog['blog'][prediction_list[0]]
-                          [responsible_idx[idx]])
-    return render_template('blog.html', prediction_text='Stress level $ {}'.format(prediction_list), responsible_atribute_for_stress=local_blog)
+    if request.method == 'GET':
+        return render_template('form.html')
+    elif request.method == 'POST':
+        responsible_idx = []
+        data = [float(x) for x in request.form.values()]
+        for idx in range(20):
+            if data[idx]-bare_min[idx] < 0:
+                responsible_idx.append(idx)
+        for idx in range(20):
+            data[idx] = (data[idx]-mn[idx])/dif[idx]
+        final_features = np.array(data).reshape(1, -1)
+        prediction = model_svm.predict(final_features)
+        prediction_list = prediction.tolist()
+        local_blog = []
+        print(len(responsible_idx))
+        for idx in range(len(responsible_idx)):
+            local_blog.append(blog['blog'][prediction_list[0]]
+                              [responsible_idx[idx]])
+        return render_template('blog.html', prediction_text='Stress level $ {}'.format(prediction_list), responsible_atribute_for_stress=local_blog)
+    else:
+        return 'methode not allowed'
 
 
 @app.route('/predict_api', methods=['POST'])
@@ -83,11 +113,10 @@ def predict_api():
     for idx in range(20):
         if data[idx]-bare_min[idx] < 0:
             res_idx.append(Attribute[idx])
-    # print(res_idx)
     for idx in range(20):
         data[idx] = (data[idx]-mn[idx])/dif[idx]
     demo = np.array(data).reshape(1, -1)
-    prediction = model.predict(demo)
+    prediction = model_svm.predict(demo)
     prediction_list = prediction.tolist()
     return jsonify({'prediction': prediction_list})
 
